dc7772ccb23596a2b3e1931a3092af480c3f1a77
Santhosh kumar <devsandy12@gmail.com>
2024-08-24 12:55:35 +0530

    mqtt impl

--- controllers/mqtt_publisher.py
+++ controllers/mqtt_publisher.py
@@ -17,21 +17,20 @@ class MqttPublisher:
         print("Message Published...", client, userdata, mid, a, b)
 
     def publish_payload(self, payload):
-        print(payload)
-        # logs_controller = LogsController()
-        # logs_controller.add_log(str(payload))
-        # settings_controller = SettingsController()
-        # settings = settings_controller.get_settings()
-        # try:
-        #     mqtt_msg = json.dumps(payload, default=str)
-        #     mqtt_host = settings['mqtt_host']
-        #     mqtt_port = int(settings['mqtt_port'])
-        #     keep_alive_interval = 45
-        #     mqtt_topic = settings['mqtt_topic']
-        #     mqtt_client = mqtt.Client(callback_api_version=mqtt.CallbackAPIVersion.VERSION2)
-        #     mqtt_client.on_publish = self.on_publish
-        #     mqtt_client.connect(mqtt_host, mqtt_port, keep_alive_interval)
-        #     mqtt_client.publish(mqtt_topic, mqtt_msg)
-        #     mqtt_client.disconnect()
-        # except Exception as exc:
-        #     logs_controller.add_log(str(exc))
+        logs_controller = LogsController()
+        logs_controller.add_log(str(payload))
+        settings_controller = SettingsController()
+        settings = settings_controller.get_settings()
+        try:
+            mqtt_msg = json.dumps(payload, default=str)
+            mqtt_host = settings['mqtt_host']
+            mqtt_port = int(settings['mqtt_port'])
+            keep_alive_interval = 45
+            mqtt_topic = settings['mqtt_topic']
+            mqtt_client = mqtt.Client(callback_api_version=mqtt.CallbackAPIVersion.VERSION2)
+            mqtt_client.on_publish = self.on_publish
+            mqtt_client.connect(mqtt_host, mqtt_port, keep_alive_interval)
+            mqtt_client.publish(mqtt_topic, mqtt_msg)
+            mqtt_client.disconnect()
+        except Exception as exc:
+            logs_controller.add_log(str(exc))
