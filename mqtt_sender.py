4c0350694322ba55e925001cf3d6bb69b33ebc6d
Santhosh kumar <devsandy12@gmail.com>
2024-08-31 18:15:53 +0530

    shifted to index

--- mqtt_sender.py
+++ mqtt_sender.py
@@ -8,18 +8,3 @@
 #     count = count + 1
 #     mqtt_conn.publish_payload({"test": count})
 #     time.sleep(120)
-
-import requests
-
-url = "https://sandbox.cashfree.com/verification/reverse-penny-drop"
-
-headers = {
-    "accept": "application/json",
-    "content-type": "application/json",
-    "x-client-id": "CF374874CPOKH02MML80HLA01FA0",
-    "x-client-secret": "cfsk_ma_test_dff80e97ff665871335028d81f249864_892a5cdb"
-}
-response = requests.post(url, headers=headers, data={"verification_id": "11", "name": "sandy"})
-
-print(response.status_code)
-print(response.text)
